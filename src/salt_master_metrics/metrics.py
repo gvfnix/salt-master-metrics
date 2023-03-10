65c8e80d4a629f470c2c120c899970c899b3c860
gvfnix <gvfnix@gmail.com>
2023-03-10 05:58:03 +0200

    issue/9[fix]: Refactor event registration & tests

--- src/salt_master_metrics/metrics.py
+++ src/salt_master_metrics/metrics.py
@@ -4,108 +4,101 @@ import salt_master_metrics.global_logger
 
 
 log = salt_master_metrics.global_logger.getLogger(__name__)
+salt_master_connected_minions_count = prometheus_client.Gauge(
+    "salt_master_connected_minions_count",
+    "Quantity of currently connected minions"
+)
+salt_master_pending_minions_count = prometheus_client.Gauge(
+    "salt_master_pending_minions_count",
+    "Quantity of minions currently pending authorization"
+)
+salt_master_function_call_count = prometheus_client.Counter(
+    "salt_master_function_call_count",
+    "How many times the function was called",
+    ["fun"]
+)
+salt_master_job_failed = prometheus_client.Counter(
+    "salt_master_job_failed",
+    "Registered job failure",
+    ["fun", "minion", "jid"]
+)
+__minions_pending = {}
+__minions_ping = {}
 
 
-minions_pending = {}
-minions_ping = {}
+def clear():
+    __minions_pending = {}
+    __minions_ping = {}
+    salt_master_connected_minions_count.set(0)
+    salt_master_pending_minions_count.set(0)
+    salt_master_function_call_count.clear()
+    salt_master_job_failed.clear()
 
 
-prometheus_metrics = {
-    "new_jobs": prometheus_client.Counter(
-        "salt_new_jobs",
-        "Total quantity of new jobs",
-        ["fun"]
-    ),
-    "minion`_failed_job": prometheus_client.Counter(
-        "salt_minion_failed_job",
-        "Failed job events",
-        ["minion", "fun", "jid"]
-    ),
-    "minions_connected": prometheus_client.Gauge(
-        "salt_minions_connected",
-        "Quantity of currently connected minions"
-    ),
-    "minions_pending": prometheus_client.Gauge(
-        "salt_minions_pending_auth",
-        "Quantity of minions pending auth"
-    )
-}
+def register_function_call(data):
+    fun = data.get("fun", "")
+    salt_master_function_call_count.labels(fun=fun).inc()
 
 
-def register_new_job(event, counter):
-    if event["tag"].endswith("/new"):
-        labels = {"fun": event["data"].get("fun", "")}
-        counter.labels(**labels).inc()
-        log.debug(f"Register new job with labels {labels}")
-
-
-def register_failed_job(event, counter):
-    if "/ret/" not in event["tag"]:
-        return
-    data = event["data"]
-    success = data["success"]
-    if success:
-        return
+def register_failed_job(data):
     labels = {
         "minion": data.get("id", ""),
         "fun": data.get("fun", ""),
         "jid": data.get("jid", "")
     }
-    counter.labels(**labels).inc()
+    salt_master_job_failed.labels(**labels).inc()
     log.debug(f"Register minion job failure with labels {labels}")
 
 
-def register_connected_minions(event, gauge, threshold:datetime.datetime=datetime.datetime.now()):
-    if event["tag"] == "salt/presence/present":
-        present_minions = event["data"].get("present", [])
-        recent_pinged_minions = filter(
-            lambda item: threshold - item[1] < datetime.timedelta(seconds=90),
-            minions_ping.items()
-        )
-        gauge_value = len(present_minions) + len(dict(recent_pinged_minions))
-        gauge.set(gauge_value)
-    if event["tag"] == "minion_ping":
-        minion_id = event["data"]["id"]
-        minions_ping[minion_id] = datetime.datetime.now()
+def register_presence(data: dict):
+    present_minions = data.get("present", [])
+    now = datetime.datetime.now()
+    recent_pinged_minions = filter(
+        lambda item: (now - item[1]) < datetime.timedelta(seconds=90),
+        __minions_ping.items()
+    )
+    _value = len(present_minions) + len(dict(recent_pinged_minions))
+    salt_master_connected_minions_count.set(_value)
 
 
-def register_pending_minions(event, gauge):
-    if event["tag"] != "salt/auth":
-        return
-    data = event["data"]
+def register_ping(data: dict):
+    now = datetime.datetime.now()
+    minion_id = data.get("id", "__none__")
+    __minions_ping[minion_id] = now
+
+
+def register_authorization(data):
     minion_id = data.get("id", "")
     action = data.get("act", "")
     if action == "pend":
-        minions_pending[minion_id] = 1
+        __minions_pending[minion_id] = 1
         log.debug(f"Added {minion_id} as pending")
-    elif action == "accept"and minion_id in minions_pending:
-        del minions_pending[minion_id]
+    elif action == "accept"and minion_id in __minions_pending:
+        del __minions_pending[minion_id]
         log.debug(f"Removed {minion_id} from pending")
-    gauge.set(len(minions_pending))
+    salt_master_pending_minions_count.set(len(__minions_pending))
 
 
-registrators = {
-    "new_jobs": register_new_job,
-    "minion_failed_job": register_failed_job,
-    "minions_connected": register_connected_minions,
-    "minions_pending": register_pending_minions,
-}
-
-
-def register_event(event, metrics=prometheus_metrics):
+def register_event(event):
     log.debug(f"Got event: {event}")
+    tag = event.get("tag", "__notag__")
+    data = event.get("data", {})
     if type(event) is not dict:
         log.debug("Event is not a dict, skipping")
         return
-    if "tag" not in event:
+    if tag == "__notag__":
         log.debug("Event has no tag, skipping")
         return
-    if "data" not in event:
+    if "data" == {}:
         log.debug("Event has no data, skipping")
         return
-    for registrator_key in registrators.keys():
-        registrator = registrators[registrator_key]
-        metric = metrics[registrator_key]
-        registrator(event, metric)
-
-
+    if tag == "salt/presence/present":
+        register_presence(data)
+    elif tag == "minion_ping":
+        register_ping(data)
+    elif tag == "salt/auth":
+        register_authorization(data)
+    elif "/ret/" in tag:
+        register_function_call(data)
+        if not data.get("success", True):
+            register_failed_job(data)
